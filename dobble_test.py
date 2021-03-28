52109de1275f96af79fb77f1a5f5fb8fe00e96d2
maxpark <maxpark0000@gmail.com>
2021-03-28 15:55:44 +0900

    Update dobble_test.py

--- dobble_test.py
+++ dobble_test.py
@@ -9,6 +9,20 @@ from keras.utils import to_categorical
 
 import dobble_utils as db
 
+import tensorflow as tf
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
+
+
 # Open dobble model
 model = load_model('dobble_model.h5')
 dir = './dobble_dataset'
